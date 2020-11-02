8c4469a3fd406802d666502db77918d322b58002
bmparedes <bmparedes5@gmail.com>
2020-11-02 17:50:32 -0400

    Bloques de codigo sencillos en PHP

--- index.php
+++ index.php
@@ -15,3 +15,17 @@ echo "Resta 2 X 2" . 2 * 2;
 echo "Resta 2 / 2" . 2 / 2;
 echo "Modulo 2 % 2" . 2 % 2;
 echo "Exponencial 2 ** 2" . 2 ** 2;
+
+// Comparacion
+// Igual ==, valor '9' == 9
+// Igual ===, valor - tipo 9 === 9 
+// Diferencias !=, valor 
+// Diferencias !==, valor - tipo
+// <, >, <=, >=
+
+// Variables variables
+$app = 'name';
+$name = 'platzi';
+
+echo $app; // name
+echo $$app; // Platzi
