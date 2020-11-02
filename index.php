c8394a75e3a2505b4ed134f04e5751ba228d209c
bmparedes <bmparedes5@gmail.com>
2020-11-02 17:10:45 -0400

    leccion sintaxis basica de php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Asignacion
+$num = 9;
+$lang = [
+    'es' => 'español',
+    'en' => 'ingles'
+];
+
+// Aritmetica
+
+echo "Suma 2 + 2" . ((int) 2 + (int) 2);
+echo "Resta 2 - 2" . ((int) 2 - (int) 2);
+echo "Resta 2 X 2" . 2 * 2;
+echo "Resta 2 / 2" . 2 / 2;
+echo "Modulo 2 % 2" . 2 % 2;
+echo "Exponencial 2 ** 2" . 2 ** 2;
