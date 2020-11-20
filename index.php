e73460a2190425571ec13c40555175a44e85be6a
bmparedes <bmparedes5@gmail.com>
2020-11-20 13:22:34 -0400

    Se creo nuevo index.php con una sintaxis mas avanzada

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
 <?php
 
-// Asignacion
-$num = 9;
-$lang = [
-    'es' => 'español',
-    'en' => 'ingles'
-];
+require __DIR__ . '/vendor/autoload.php';
 
-// Aritmetica
-
-echo "Suma 2 + 2" . ((int) 2 + (int) 2);
-echo "Resta 2 - 2" . ((int) 2 - (int) 2);
-echo "Resta 2 X 2" . 2 * 2;
-echo "Resta 2 / 2" . 2 / 2;
-echo "Modulo 2 % 2" . 2 % 2;
-echo "Exponencial 2 ** 2" . 2 ** 2;
-
-// Comparacion
-// Igual ==, valor '9' == 9
-// Igual ===, valor - tipo 9 === 9 
-// Diferencias !=, valor 
-// Diferencias !==, valor - tipo
-// <, >, <=, >=
-
-// Variables variables
-$app = 'name';
-$name = 'platzi';
-
-echo $app; // name
-echo $$app; // Platzi
+echo upper('Hola');
