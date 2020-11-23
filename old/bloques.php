67bda380c19cbf3acd8d956605395d53e40c5817
bmparedes <bmparedes5@gmail.com>
2020-11-23 17:41:24 -0400

    aqui esta los comandos de inicio de curso, y 'proyecto antiguo'

---
+++ old/bloques.php
@@ -0,0 +1,42 @@
+<?php
+
+if (condition){
+     # code...
+    } else   {
+     # code... 
+    }
+
+switch ($login) {
+    case true;
+        # code...
+    break;
+    case false;
+        # code...
+    break;
+    default;
+        # code...
+    break;
+
+}
+// Consulta, usuario, 
+foreach ($datos as $id => $name) {
+    # code...
+}
+// Se puede omitir la key
+foreach ($datos as $row) {
+    # code...
+}
+$a = 1;
+while ($a <= 10){
+    # code...
+
+    $a++;
+}
+
+do {
+    # code...
+} while ($a <= 10);
+
+for ($i=0; $i < 10; $i++) { 
+    # code...
+}
---
+++ old/src/Format.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Text;
+
+// El estandar correcto, el nombre de la clase debe ser el nombre del archivo sin la extension php
+class Format
+{
+    public static function upperText($value)
+    {
+        return strtoupper($value);
+    }
+    public static function lowerText($value)
+    {
+        return strtolower($value);
+    }
+}
